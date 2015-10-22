cf02a02227150432ac4be9914cc7dc9c0262fff6
SinwarPrem <premsinwar4@gmail.com>
2015-10-23 04:39:41 +0530

    quicksort added

--- Sorting/quickSort.cpp
+++ Sorting/quickSort.cpp
@@ -4,11 +4,49 @@
 	*/
 #include<stdio.h>
 #include<conio.h>
-//function for quickSorting
-void quicksort(int *a, int n)
+#include<stdlib.h>
+//it sorts array such that the elements less than pivot are left side(one side) of array 
+int partition(int a[], int p, int r)
 {
-	
+  int i, j, pivot, temp;
+  pivot = a[p];
+  i = p;
+  j = r;
+  while(1)
+  {
+   while(a[i] < pivot && a[i] != pivot)
+   i++;//increament in pivot index if element less than pivot element
+   while(a[j] > pivot && a[j] != pivot)
+   j--;//vice versa of above comment
+   if(i < j)//swap elements so that lesser elements come one side
+   {
+    temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+   }
+   else
+   {
+    return j;
+   }
+  }
 }
+
+
+//function for quickSorting
+//p and q are start and end indexes of a array to be sorted
+
+void quicksort(int *a, int p,int r)
+{
+	int q;
+	if(p<r)
+	{
+		q=partition(a,p,r); //calling partition function
+		quicksort(a,p,q-1);
+		quicksort(a,q+1,r);
+	}
+}
+
+
 int main()
 {
 	int n,a[1000],i;
@@ -18,9 +56,10 @@ int main()
 	for(i=0;i<n;i++)
 	scanf("%d",&a[i]);
 	//calling quicksort function
-	quicksort(a,n);
-	printf("sorted elements by quickesort\n");
+	quicksort(a,0,n-1);
+	printf("sorted elements by quicksort\n");
 	for(i=0;i<n;i++)
 	printf("%d ",a[i]);
 	return 0;
 }
+
